6869e4fccc3bfe47ad6e25ebd49e68801a71ad4e
riddlewiggler <riddlewiggler@protonmail.com>
2023-11-12 18:02:00 +0100

    ft/86bwdg7zq: ✨ Added return value to function signature

--- src/libs/init_logger.rs
+++ src/libs/init_logger.rs
@@ -2,15 +2,17 @@ use env_logger::Builder;
 use log::LevelFilter;
 
 /// Init env_logger with `verbose` CLI arg support
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `is_verbose` - true if the verbose CLI arg is set
-pub fn init_logger(is_verbose: bool) {
+pub fn init_logger(is_verbose: bool) -> String {
     if is_verbose == false {
         env_logger::init();
+        return "env".to_string();
     } else {
         Builder::new().filter_level(LevelFilter::max()).init();
+        return "verbose".to_string();
     }
 }
 
